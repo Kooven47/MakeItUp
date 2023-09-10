afc7b38a517321efa85b2523a0ea0bc32936d63d
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-09-10 18:50:56 -0400

    Added attack system
    Added attack system animation wise but no combat system just yet.

--- Assets/Scripts/PlayerScripts/NormalAttack.cs
+++ Assets/Scripts/PlayerScripts/NormalAttack.cs
@@ -5,15 +5,56 @@ using UnityEngine;
 public class NormalAttack : MonoBehaviour
 {
     [SerializeField]private Animator _anim;
+    bool _attackBuffer = false, _inAttack = false;
+    [SerializeField]int _chain = 0;
     // Start is called before the first frame update
     void Start()
     {
+        _anim = GetComponent<Animator>();
         
+    }
+
+    void Recover()
+    {
+        if (_attackBuffer)
+        {
+            _attackBuffer = false;
+            Attack();
+        }
+        else
+        {
+            Debug.Log("End Chain");
+            _inAttack = false;
+            _chain = 0;
+            _anim.SetInteger("chain",_chain);
+
+        }
+    }
+
+    void Attack()
+    {
+        _chain++;
+        if (_chain > 3)
+            _chain = 1;
+        
+        _anim.SetInteger("chain",_chain);
+        _inAttack = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp("z"))
+        {
+            if (!_inAttack)
+            {
+                Attack();
+            }
+            else
+            {
+                _attackBuffer = true;
+            }
+                
+        }
     }
 }
