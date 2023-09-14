b988d9423f89119735c061b72c0f75d3cb0b7a3b
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-09-13 21:24:56 -0400

    Added hitstop and Screen Shake. Redid Janitor sprites.
    Added hitstop and screen shake. Redid the normal attacks to use new scriptable objects which in turn allows for more modular animator.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -9,7 +11,46 @@ public class CameraFollow : MonoBehaviour
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
+    // Allows for the function to be called by any other script without needing a reference to the class
+    public static Action StartShake;
+
     [SerializeField] private Transform target;
+    [SerializeField] private CinemachineVirtualCamera _cinemaVirtualCam;
+    
+    CinemachineBasicMultiChannelPerlin multiChannelPerlin;
+
+    Coroutine _shakeTimer;
+
+    void Start()
+    {
+        multiChannelPerlin = _cinemaVirtualCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        StartShake = CameraShake;
+    }
+
+    private IEnumerator ShakeTimer(float time, float intensity)
+    {
+        float timeTotal = time;
+        while (time > 0f)
+        {
+            yield return new WaitForSecondsRealtime(Time.deltaTime);
+            time -= Time.deltaTime;
+            multiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(intensity,0f,(1-(time/timeTotal)));
+        }
+        _shakeTimer = null;
+    }
+
+    private void CameraShake()
+    {
+        if (_shakeTimer != null)
+        {
+            StopCoroutine(_shakeTimer);
+        }
+
+        // Dictates intensity of shake. Though try 500 in runtime mode. I promise it's fun ;3
+        multiChannelPerlin.m_AmplitudeGain = 5f;
+
+        _shakeTimer = StartCoroutine(ShakeTimer(0.1f,5f));
+    }
 
     // Update is called once per frame
     private void FixedUpdate()
