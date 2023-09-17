4fbf494cd92ede1e296bbd2c451be761c88e1b19
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-09-17 18:18:40 -0400

    Health Bar Display Created
    Created health display that updates the healthbar segments based on HP ratio both going up and down. Added toilet paper sprites for health.

---
+++ Assets/Scripts/PlayerScripts/HealthBar.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]private int _maxSegments = 0;
+    private float _curRatio = 1f;
+
+    private float _testCurHP = 100f, _testMaxHP = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _maxSegments = transform.childCount;
+    }
+
+    void SetHealth(float curHP, float maxHP)
+    {
+        if (curHP < 0f)
+        {
+            curHP = 0f;
+        }
+        else if (curHP > maxHP)
+        {
+            curHP = maxHP;
+        }
+
+        // 0.8f new ratio, old ratio = 0.67f
+        float newRatio = (curHP/maxHP);
+
+        if (newRatio == _curRatio)
+            return;
+
+        float curSegments = _curRatio * _maxSegments;
+        float newSegments = newRatio * _maxSegments;
+
+        bool isIncreasing = newRatio > _curRatio;
+
+        int newHPSegment = (int)Mathf.Ceil(newSegments);
+
+        Debug.Log("CurSegment "+curSegments);
+        Debug.Log("newSegment "+newSegments);
+
+        float diff = newSegments - Mathf.Floor(newSegments);
+
+        if (!isIncreasing)
+        {
+            
+            for (int i = 0; i < _maxSegments; i++)
+            {
+                if (i == (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = diff;
+                }
+                else if (i > (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = 0.0f;
+                }
+            }  
+        }
+        else
+        {
+            // Insert HP increasing code for when restoring health.
+            
+            Debug.Log("Diff is "+diff);
+            
+            for (int i = 0; i < _maxSegments; i++)
+            {
+                if (i == (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = (diff <= 0f ? 1.0f : diff);
+                }
+                else if (i < (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = 1.0f;
+                }
+            } 
+        }
+        
+
+        _curRatio = newRatio;
+
+        _testCurHP = curHP;
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp("x"))
+        {
+            SetHealth(_testCurHP - 7f,_testMaxHP);
+        }
+        if (Input.GetKeyUp("c"))
+        {
+            SetHealth(_testCurHP + 7f,_testMaxHP);
+        }
+    }
+}
