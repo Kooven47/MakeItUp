fd4db0231eb4be7e2eeb4927849494712d3cf6a7
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-09-30 15:06:33 -0400

    Squashed commit of the following:
    commit 34beb6ae929449c500aaca555019de254b128f4d
    Author: Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
    Date:   Sun Sep 17 18:50:46 2023 -0400
    
        Revert "Merge branch 'main' into Rayyan-Dev"
    
        This reverts commit 8e3737e10d9168b2793dbaf19dc222eda7268928, reversing
        changes made to ccae19647c038b5a0fd6f95a33555baeec125c35.
    
    commit 10460769f27fdd9abc2de7963f1b1b4d928f6e61
    Author: Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
    Date:   Sun Sep 17 18:48:49 2023 -0400
    
        Revert "Updated healthbar code to add comments"
    
        This reverts commit ccae19647c038b5a0fd6f95a33555baeec125c35.
    
    commit 2fb69325c06c37eed907eb1f2acfc66194aacaf0
    Merge: 706d21c 8e3737e
    Author: Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
    Date:   Sun Sep 17 18:38:59 2023 -0400
    
        Merge pull request #10 from Kooven47/Rayyan-Dev
    
        Rayyan dev. Adding in healthbar display code.

--- Assets/Scripts/PlayerScripts/HealthBar.cs
+++ Assets/Scripts/PlayerScripts/HealthBar.cs
@@ -17,7 +17,6 @@ public class HealthBar : MonoBehaviour
 
     void SetHealth(float curHP, float maxHP)
     {
-        // For test purposes. Later on the HP management will be done by stat script.
         if (curHP < 0f)
         {
             curHP = 0f;
@@ -27,75 +26,62 @@ public class HealthBar : MonoBehaviour
             curHP = maxHP;
         }
 
-        // Gets new ratio from passed in current HP and max HP
+        // 0.8f new ratio, old ratio = 0.67f
         float newRatio = (curHP/maxHP);
 
-        // If no change, just return
         if (newRatio == _curRatio)
             return;
 
-        // Gathers the number of segments from the current ratio and new ratio
         float curSegments = _curRatio * _maxSegments;
         float newSegments = newRatio * _maxSegments;
 
-        // Check to see if the new ratio is greater than the old ratio
         bool isIncreasing = newRatio > _curRatio;
 
-        // Check which segment will get the fillAmount. Example: 4.75 segments means 5th segment will get fillAmount
         int newHPSegment = (int)Mathf.Ceil(newSegments);
 
         Debug.Log("CurSegment "+curSegments);
         Debug.Log("newSegment "+newSegments);
 
-        // Gets fill amount by subtracting the segment count by the floor. Example: 4.75 - 4<-(Floor(4.75)) = 0.75 fillamount
         float diff = newSegments - Mathf.Floor(newSegments);
 
-        if (isIncreasing)
+        if (!isIncreasing)
         {
-            // Insert HP increasing code for when restoring health.
             
-            Debug.Log("Diff is "+diff);
-            
-            for (int i = 0; i < _maxSegments; i++)// Iterate through all segments.
-            {
-                // Due to child counting starting at 0. Example: 5th segment means Child #4
-                if (i == (newHPSegment - 1))
-                {
-                    // If we get a differnce of 0 (going to whole number) just have fillAmount be 1f
-                    // Once we reach that sprite, end loop.
-                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = (diff <= 0f ? 1.0f : diff);
-                    break;
-                }
-                else if (i < (newHPSegment - 1))
-                {
-                    // Any segments BEFORE the selected segment will automtically have fillAmount of 1f
-                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = 1.0f;
-                }
-            } 
-        }
-        else
-        {
-            for (int i = 0; i < _maxSegments; i++)// Iterate through all segments
+            for (int i = 0; i < _maxSegments; i++)
             {
                 if (i == (newHPSegment - 1))
                 {
                     transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = diff;
                 }
-                else if (i > (newHPSegment - 1))// If the segments selected are AFTER the selected segment, set it to 0 fillAmount
+                else if (i > (newHPSegment - 1))
                 {
-                    Image toiletSprite = transform.GetChild(i).GetChild(0).GetComponent<Image>();
-
-                    if (toiletSprite.fillAmount == 0.0f)// If the sprite fill already is empty, end loop
-                        break;
-                    
-                    toiletSprite.fillAmount = 0.0f;
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = 0.0f;
                 }
             }  
         }
+        else
+        {
+            // Insert HP increasing code for when restoring health.
+            
+            Debug.Log("Diff is "+diff);
+            
+            for (int i = 0; i < _maxSegments; i++)
+            {
+                if (i == (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = (diff <= 0f ? 1.0f : diff);
+                }
+                else if (i < (newHPSegment - 1))
+                {
+                    transform.GetChild(i).GetChild(0).GetComponent<Image>().fillAmount = 1.0f;
+                }
+            } 
+        }
+        
 
         _curRatio = newRatio;
 
-        _testCurHP = curHP;// For test purposes. Later on the HP management will be done by stat script.
+        _testCurHP = curHP;
         
     }
 
