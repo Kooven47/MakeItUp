9b091981898ef6f9667f72fee459e36b05b00dc5
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-11-27 22:01:19 -0500

    Final Boss assets in.

--- Assets/Scripts/FinalBossScript.cs
+++ Assets/Scripts/FinalBossScript.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class FinalBossScript : MonoBehaviour
+public class FinalBossScript : EnemyCore
 {
     // Bosscore?
     protected EnemyStats _bossStats;
     protected EnemyAI _bossAI;
     protected Rigidbody2D _bossRB;
-    protected EnemyCore _bossCore;
     protected SpriteRenderer _bossSpriteRender;
     protected GameObject _bossGameObject;
+    [SerializeField] private GameObject _bossOrb;
+    [SerializeField] private Sprite[] _explosioneffect = new Sprite[3];
+
     protected Transform _bossBubbleEmitter;
     protected Transform _bossDustEmitter;
     private SpawnManager spawnManager;
@@ -24,8 +26,9 @@ public class FinalBossScript : MonoBehaviour
     [SerializeField] private Material defaultOutline;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
+        base.Start();
         EnemyStats.OnDeathWithType += KillUpdate;
         minionLocations = new List<Transform>();
         foreach (Transform child in GameObject.Find("GruLocations").GetComponent<Transform>())
@@ -34,12 +37,12 @@ public class FinalBossScript : MonoBehaviour
         }
         spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         numMinions = minionLocations.Count;
-        _bossStats = GetComponent<EnemyStats>();
-        _bossAI = GetComponent<EnemyAI>();
-        _bossRB = GetComponent<Rigidbody2D>();
-        _bossCore = GetComponent<EnemyCore>();
-        _bossGameObject = this.gameObject;
-        _bossSpriteRender = _bossGameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        _bossGameObject = transform.parent.gameObject;
+        _bossStats = _bossGameObject.GetComponent<EnemyStats>();
+        _bossAI = _bossGameObject.GetComponent<EnemyAI>();
+        _bossRB = _bossGameObject.GetComponent<Rigidbody2D>();
+        
+        _bossSpriteRender = GetComponent<SpriteRenderer>();
         _bossBubbleEmitter = _bossGameObject.transform.GetChild(2);
         _bossDustEmitter = _bossGameObject.transform.GetChild(3);
         SpawnMinions();
@@ -49,10 +52,23 @@ public class FinalBossScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_bossStats.healthRatio <= 0.5f)
+        // if (_bossStats.healthRatio <= 0.5f)
+        // {
+        //     _bossRB.gravityScale = 0;
+        //     _bossAI.flyingEnabled = true;
+        // }
+
+        if (_canAttack)
         {
-            _bossRB.gravityScale = 0;
-            _bossAI.flyingEnabled = true;
+            SelectAttack();
+            if (_attackIndex == 0)
+            {
+                ReadyAttack();
+            }
+            else if (_attackIndex == 1)
+            {
+                ChargingAttacks(5f,2f);
+            }
         }
     }
 
@@ -132,6 +148,64 @@ public class FinalBossScript : MonoBehaviour
             _bossSpriteRender.material = defaultOutline;
             _bossBubbleEmitter.gameObject.SetActive(false);
             _bossDustEmitter.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void MeleeStrike()
+    {
+        List<Collider2D> targets = new List<Collider2D>();
+        _hurtBox.gameObject.SetActive(true);
+        Physics2D.OverlapCollider(_hurtBox,_hurtLayers,targets);
+        _hurtBox.gameObject.SetActive(false);
+        bool didHit = false;
+
+        PlayerStats _playerStat;
+
+        if (targets.Count != 0)
+        {
+            foreach(Collider2D col in targets)
+            {
+                if (col.CompareTag("Player"))
+                {
+                    Debug.Log("Hit the janitor!");
+                    _playerStat = col.GetComponent<PlayerStats>();
+                    if (!_playerStat.iFrame)
+                    {
+                        Vector2 direction = (col.transform.position - transform.position).normalized;
+                        col.GetComponent<PlayerInterrupt>().Stagger(1,_knockBackVector * direction * 0.5f);
+                        _playerStat.DamageCalc(_enemySkills[_attackIndex].damage,_bossStats.Attribute,false);
+                        col.gameObject.GetComponent<DamageEffect>().TriggerEffect((int)_bossStats.Attribute);
+                        didHit = true;
+                    }
+                    else
+                    {
+                        Debug.Log("Under Iframes");
+                    }
+                }
+            }
+        }
+
+        if (didHit)
+        {
+            // Question mark acts as a null check to avoid invoking an action if not initialized somehow
+            CameraFollow.StartShake?.Invoke();
+        }
+    }
+
+    protected override void SelectAttack()
+    {
+        
+        if (InDistance(_meleeRange))
+        {
+            _canAttack = false;
+            _attackIndex = 0;
+            _knockBackVector = EnumLib.KnockbackVector(_enemySkills[_attackIndex].force);
+        }
+        else if (InDistance(_projectileRange))
+        {
+            _canAttack = false;
+            _attackIndex = 1;
+            _knockBackVector = EnumLib.KnockbackVector(_enemySkills[_attackIndex].force);
         }
     }
 }
