dd7277a449cb9781d9f46a1a4c748ae5c5a56704
Kevin <74631846+Kooven47@users.noreply.github.com>
2023-11-28 18:58:48 -0500

    Add getting hit sound effect

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 public class Elevator : MonoBehaviour
 {
     public Animator anim;
+    private PlayerControllerJanitor _playerControllerJanitor;
+    
+    private void Start()
+    {
+        _playerControllerJanitor = GameObject.FindWithTag("Player").GetComponent<PlayerControllerJanitor>();
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            const int ELEVATOR = 8;
-            GameObject.FindWithTag("Player").GetComponent<PlayerControllerJanitor>().PlaySoundEffect(ELEVATOR);
+            const int ELEVATOR = 9;
+            _playerControllerJanitor.PlaySoundEffect(ELEVATOR);
             StartCoroutine(Advance());
         }
     }
