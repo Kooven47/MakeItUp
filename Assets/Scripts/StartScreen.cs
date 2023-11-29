d660d04048e9b466ab4de0a34e3f6ce9f5a513f7
Rhaque20 <72638964+Rhaque20@users.noreply.github.com>
2023-11-28 19:17:03 -0500

    Implement saving on start menu and additional sprites.

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartScreen : MonoBehaviour
+public class StartScreen : MonoBehaviour,ISaveGame
 {
     public void NewGame()
     {
+        SaveSystem.instance.SaveGame();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadGame()
     {
         // TODO: INSERT LOADING FROM SAVE FUNCTIONALITY
+        SaveSystem.instance.LoadGame();
         Debug.Log("Pressed load game!");
     }
 
@@ -24,5 +26,25 @@ public class StartScreen : MonoBehaviour
     {
         Debug.Log("Pressed quit game!");
         Application.Quit();
+    }
+
+    public void LoadSaveData(SaveData data)
+    {
+        SceneManager.LoadScene(data.currentLevel);
+    }
+
+    public void LoadInitialData(SaveData data)
+    {
+        SceneManager.LoadScene(data.currentLevel);
+    }
+
+    public void SaveData(ref SaveData data)
+    {
+        data.currentLevel = SceneManager.GetActiveScene().buildIndex + 2;
+    }
+
+    public void SaveInitialData(ref SaveData data)
+    {
+        data.currentLevel = SceneManager.GetActiveScene().buildIndex + 2;
     }
 }
