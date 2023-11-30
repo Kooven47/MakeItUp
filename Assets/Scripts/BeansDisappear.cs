ee3b12ede261a231184d451e1b0a9ccaefb61be0
EthanAdkins <102610253+EthanAdkins@users.noreply.github.com>
2023-11-30 15:46:56 -0500

    Changed fridge spawn location, fixed beans bug

--- Assets/Scripts/BeansDisappear.cs
+++ Assets/Scripts/BeansDisappear.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BeansDisappear : MonoBehaviour
 {
     [SerializeField] private GameObject _beans;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
